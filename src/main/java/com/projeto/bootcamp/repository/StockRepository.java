878b2e0d3496e1789ff16a1959ad823126ef2e6a
Silva Gabriel Martins <gabriel.silva12@estudante.ufla.br>
2021-05-29 20:39:32 -0300

    Adicionado Repository

---
+++ src/main/java/com/projeto/bootcamp/repository/StockRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.bootcamp.repository;
+
+import com.projeto.bootcamp.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
+}
